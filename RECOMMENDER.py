e2107037b3cc237211bdadaeca772280cec47e5c
Chris Johnson <43188404+cjohnson3585@users.noreply.github.com>
2020-01-30 14:35:11 -0500

    Add files via upload

--- RECOMMENDER.py
+++ RECOMMENDER.py
@@ -1,49 +1,74 @@
 from KNN_ALGO import knn, euclidean_distance
+import sys
+import numpy as np
+import glob
 
-def recommend_movies(movie_query, k_recommendations):
-    raw_movies_data = []
-    with open('patents_recommendation_data.csv', 'r') as md:
-#    with open('movies_recommendation_data.csv', 'r') as md:
+
+homedir = './PATENT_DIR/'
+
+
+
+def recommend_pats(query, k_recommendations):
+    raw_pats_data = []
+    with open('./export_patents.csv', 'r') as md:
         # Discard the first line (headings)
         next(md)
-
+        next(md)
         # Read the data into memory
         for line in md.readlines():
             data_row = line.strip().split(',')
-            raw_movies_data.append(data_row)
+            raw_pats_data.append(data_row)
 
     # Prepare the data for use in the knn algorithm by picking
     # the relevant columns and converting the numeric columns
     # to numbers since they were read in as strings
-    movies_recommendation_data = []
-    for row in raw_movies_data:
-        data_row = list(map(float, row[1:]))
-        movies_recommendation_data.append(data_row)
+    pats_recommendation_data = []
+    for row in raw_pats_data:
+#        data_row = list(map(float, row[1:]))
+        data_row = [float(i) for i in row[1:]]
+#        print(data_row)
+        pats_recommendation_data.append(data_row)
         
-    print(movies_recommendation_data)
-
-    # Use the KNN algorithm to get the 5 movies that are most
-    # similar to The Post.
+    # Use the KNN algorithm to get the K patentss that are most
+    # similar to target.
     recommendation_indices, _ = knn(
-        movies_recommendation_data, movie_query, k=k_recommendations,
+        pats_recommendation_data, query, k=k_recommendations,
         distance_fn=euclidean_distance, choice_fn=lambda x: None
     )
 
-    movie_recommendations = []
+    pats_recommendations = []
     for _, index in recommendation_indices:
-        movie_recommendations.append(raw_movies_data[index])
+        pats_recommendations.append(raw_pats_data[index])
+    md.close()
+    return pats_recommendations
 
-    return movie_recommendations
+#-------------------------------------------------------
+#Get target data function
+tar_file = glob.glob(homedir+"target_*.csv")
+target_row_data = []
 
-if __name__ == '__main__':
-    target_patent_kw_freq = [460.0, 335.0, 306.0, 165.0, 103.0, 101.0, 95.0, 77.0, 72.0, 70.0, 54.0, 51.0, 48.0, 45.0, 42.0] # feature vector for The Post
-#    target_patent_kws=[traffic, signal, vehicle, plurality, data, period, state, communication, signals, location, modification, travel, information, network, electronic]
-    target_patent = ['10115305', 'Optimizing autonomous car"\'"s driving time and user experience using traffic signal information']
+with open('./export_patents.csv', 'r') as mdd:
+        next(mdd)
+        next(mdd)
+        for line in mdd.readlines():
+            target_row = line.strip().split(',')
+            target_row_data.append(target_row)
+            break
+mdd.close()
 
+target_patent_kw_freq = [float(i) for i in target_row_data[0][1:]]
+target_patent = target_row_data[0][0]
 
-    recommended_movies = recommend_movies(movie_query=target_patent_kw_freq, k_recommendations=5)
+print('')
+print('RECOMMENDER.py-------------')
+print('Target Patent: ',target_patent)
+print('Target KW Freq: ', target_patent_kw_freq)
+          
+#pass to recommend_pats
+recommended_pats = recommend_pats(query=target_patent_kw_freq, k_recommendations=6)
 
     # Print recommended movie titles
-    print('Target Patent Number:', target_patent[0])
-    for recommendation in recommended_movies:
-        print(recommendation[0])
+print('')
+print('Recommendations:')
+for recommendation in recommended_pats:
+    print(recommendation[0])
